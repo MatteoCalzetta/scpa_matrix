44e4044cb41b1609ff22f3f5ebc3dfaf87981b55
Alessandro <alessandrolori179@gmail.com>
2025-03-05 18:01:05 +0100

    Corretto errore CLOCK

--- src/matrmult.c
+++ src/matrmult.c
@@ -5,6 +5,8 @@
 #include "../include/csr_matrix.h"
 #include "../include/matrmult.h"
 
+
+/*
 // Funzione per il prodotto matrice-vettore BASE in CSR
 double csr_matrtimesvect(CSRMatrix *csr, int *x, double *y) {
     struct timespec start, end;
@@ -19,4 +21,26 @@ double csr_matrtimesvect(CSRMatrix *csr, int *x, double *y) {
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
     
     return (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+}
+*/
+
+
+// Funzione per il prodotto matrice-vettore BASE in CSR
+double csr_matrtimesvect(CSRMatrix *csr, int *x, double *y) {
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();  // Avvia il timer
+
+    for (int i = 0; i < csr->M; i++) {
+        for (int j = csr->IRP[i]; j < csr->IRP[i + 1]; j++) {
+            y[i] += csr->AS[j] * x[csr->JA[j]];
+        }
+    }
+
+    end = clock();  // Ferma il timer
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;  // Calcola il tempo in secondi
+
+    return cpu_time_used;
 }
